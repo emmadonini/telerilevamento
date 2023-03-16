2a07ae89be9a9b0da594e3d7a7d971a005624bcc
emmadonini <100956835+emmadonini@users.noreply.github.com>
2023-03-16 11:07:07 +0100

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -50,12 +50,40 @@ freq(l92c$map)
 # classe 1 con 34226 pixels (aree agricole)
 # classe 2 con 307066 pixels (foresta)
 
+tot92 <- 341292
+
+# proporzione delle classi
+prop_forest_92 <- 307066 / tot92
+
+# percentuale delle classi
+perc_forest_92 <- 307066 * 100 / tot92 
+
+# percentuale di aree agricole
+perc_agr_92 <- 34226 * 100 / tot92 
+# oppure 
+# perc_agr_92 <- 100 - perc_forest_92
+
+# perc_forest_92 : 89.97164
+# perc_agr_92 : 10.02836
+
+
 freq(l06c$map)
 # classe 1 con 177913 pixels (foresta)
 # classe 2 con 164813 pixels (aree agricole)
 
+tot_06 <- 342726
+percent_forest_06 <- 177913 * 100 / tot_06
+percent_agr_06 <- 100 - percent_forest_06
 
+# percent_forest_06 : 51.91115
+# percent_agr_06 : 48.08885
 
+# Dati finali:
+
+# perc_forest_92 : 89.97164
+# perc_agr_92 : 10.02836
+# percent_forest_06 : 51.91115
+# percent_agr_06 : 48.08885
 
 
 
